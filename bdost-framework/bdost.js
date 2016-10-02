19d7da11038bc9939003d31c38a3125c6cd6512f
aekynr <ae@alperen.pro>
2016-10-02 21:09:45 +0300

    changes

--- bdost-framework/bdost.js
+++ bdost-framework/bdost.js
@@ -885,7 +885,7 @@ function stepOne(senderID, messageText){
     
     if(fd.firstVar){
       if(fd.firstVar === "Evet"){
-        fd.step = 1;
+        fd.step = 2;
       }
     }        
   }
@@ -895,9 +895,9 @@ function stepOne(senderID, messageText){
     fd.variables.questionOne = messageText;
     
     if(fd.variables.quesionOne === ""){
-      fd.step = 1;
-    }else{
       fd.step = 2;
+    }else{
+      fd.step = 3;
     }
   }
 

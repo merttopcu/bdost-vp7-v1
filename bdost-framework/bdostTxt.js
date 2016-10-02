d1e1fdc5c5bdf6ff3bf62c5dbb799d2ef5608729
aekynr <ae@alperen.pro>
2016-10-02 15:50:08 +0300

    changes

--- bdost-framework/bdostTxt.js
+++ bdost-framework/bdostTxt.js
@@ -11,9 +11,9 @@ exports.txtAgain			= productName +" tekrar iş başında :)";
 exports.txtBye		 		= "Kariyer hayatında başarılar dilerim. :)";
 
 // 2 + 1 model
-exports.modelOne	 		= "İş Arıyorum";
+exports.modelOne	 		= "Telefon";
 exports.modelTwo	 		= "Şifremi Unuttum :(";
-exports.modelPlus	 		= "Yetkili Arıyorum";
+exports.modelPlus	 		= "Yetkili";
 
 //----------------- Model One -----------------
 exports.MOWelcome  			= "Uygun iş fırsatlarını seninle paylaşıp, sana yardım etmek istiyorum.";

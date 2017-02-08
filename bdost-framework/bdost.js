53de4dd070a280e1f4df19c3170546e22d011563
NMT <merttopcu@gmail.com>
2017-02-07 20:37:33 -0800

    fix gentemp

--- bdost-framework/bdost.js
+++ bdost-framework/bdost.js
@@ -584,38 +584,6 @@ function stepOne(senderID, messageText){
     }else{
 
       facebook.sendTextMessage(senderID,bdostTxt.MOResult,fd.db);
-      var g_attachment = {
-        "type":"template",
-        "payload":{
-          "template_type":"generic",
-          "elements":[
-            {
-              "title":"Welcome to Peter\'s Hats",
-              "image_url":"https://petersfancybrownhats.com/company_image.png",
-              "subtitle":"We\'ve got the right hat for everyone.",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                "messenger_extensions": true,
-                "webview_height_ratio": "tall",
-                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                },
-              "buttons":[
-                {
-                  "type":"web_url",
-                  "url":"https://petersfancybrownhats.com",
-                  "title":"View Website"
-                },{
-                  "type":"postback",
-                  "title":"Start Chatting",
-                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                }              
-              ]      
-            }
-          ]
-        }
-      };
-      console.log("attachment: ", g_attachment);
 
       setTimeout(function() {
         facebook.sendTextMessage(senderID,bdostTxt.MOSearchMessage,fd.db);
@@ -624,7 +592,39 @@ function stepOne(senderID, messageText){
         facebook.sendTextMessage(senderID,"API goes here.",fd.db);
         facebook.sendTextMessage(senderID,"a1:"+fd.qOne+" a2:"+fd.qTwo+" a3:"+fd.qThree+" a4:"+fd.qFour);
         // insert generic template function sendGenericMessage(recipientId,attachment)
-        facebook.sendGenericMessage(senderID, g_attachment);
+        var g_attachment = {
+          "type":"template",
+          "payload":{
+            "template_type":"generic",
+            "elements":[
+              {
+                "title":"Welcome to Peter\'s Hats",
+                "image_url":"https://petersfancybrownhats.com/company_image.png",
+                "subtitle":"We\'ve got the right hat for everyone.",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                  },
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://petersfancybrownhats.com",
+                    "title":"View Website"
+                  },{
+                    "type":"postback",
+                    "title":"Start Chatting",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                  }              
+                ]      
+              }
+            ]
+          }
+        };
+      console.log("attachment: ", g_attachment);
+      facebook.sendGenericMessage(senderID, g_attachment);
       }, 2000)
     
       setTimeout(function() {
